a929c05e4f4fa7d76f49623c313454fd0eb9edd3
Rustam Herheman <gergemannn@gmail.com>
2026-05-10 18:29:29 +0300

    fix issues 5

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,6 +1,5 @@
 export const Sum = ({ a = 0, b = 0 }) => (
-  // eslint-disable-next-line prettier/prettier
   <p>
-    {`Sum of ${a} and ${b} is ${a + b}`}
-    </p>
+    Sum of {a} and {b} is {a + b}
+  </p>
 );
